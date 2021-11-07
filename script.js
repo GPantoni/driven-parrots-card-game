d629a4254e69bc822a37cc236c4e85077c08a662
Glauco Pantoni <glaucopantoni@gmail.com>
2021-11-07 19:43:05 -0300

    Functions básicas JS

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function cardsAmount() {
         nCards = prompt('Informe o número de cartas.(Escolha um número par de 4 a 14');
     }
 }
+
+function clock() {
+    let nTime = document.querySelector('.timer');
+    timer++;
+    nTime.innerHTML = `${timer}s`;
+}
+
+function move() {
+    let moves = document.querySelector('.moves');
+    totalMoves++;
+    moves.innerHTML = `${totalMoves}`
+}
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+function start() {
+    cardsAmount()
+
+    
+}
