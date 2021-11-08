6384f1db23a1eba93c51b7480f32d51aed7f81a4
Glauco Pantoni <glaucopantoni@gmail.com>
2021-11-07 22:06:12 -0300

    JS - gameDeck

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const nCards = 0;
+let cards = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot'
+]
+let nCards = 0;
 let timer = 0;
 let totalMoves = 0;
+let intervalId;
 
 function cardsAmount() {
-    nCards = prompt('Informe o número de cartas.(Escolha um número par de 4 a 14');
-
     while(nCards < 4 || nCards > 14 || nCards % 2 !== 0) {
-        nCards = prompt('Informe o número de cartas.(Escolha um número par de 4 a 14');
+        nCards = Number(prompt('Informe o número de cartas.(Escolha um número par de 4 a 14)'));
     }
 }
 
@@ -19,15 +27,43 @@ function clock() {
 function move() {
     let moves = document.querySelector('.moves');
     totalMoves++;
-    moves.innerHTML = `${totalMoves}`
+    moves.innerHTML = `${totalMoves}`;
 }
 
 function comparador() {
     return Math.random() - 0.5;
 }
 
-function start() {
-    cardsAmount()
+function gameDeck() {
+    let deck = [];
+    cards.sort(comparador);
+    for(let i = 0; i < nCards/2; i++) {
+        deck.push(cards[i]);
+        deck.push(cards[i]);
+    }
+    deck.sort(comparador);
 
+    return deck;
+}
+
+function setCardTable() {
+    let table = document.querySelector('.main');
+
+    table.innerHTML += `
+        <div class="card">
+            <div class="back-face face">
+                <img src="/assets/front.png" alt="">
+            </div>
+            <div class="front-face face">
+                <img src="/assets/bobrossparrot.gif" alt="">
+            </div>
+        </div>
+        `
+}
+
+function start() {
+    cardsAmount();
+
+    intervalId = setInterval(clock, 1000);
     
 }
